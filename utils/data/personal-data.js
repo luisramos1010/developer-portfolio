c2408d477411d54df3ceb62241bd2c103ec2fc9f
Superstar051010 <Superstar051010@gmail.com>
2025-08-23 17:24:31 -0700

    Project init

---
+++ utils/data/personal-data.js
@@ -0,0 +1,13 @@
+export const personalData = {
+  name: "Alexander Rowan Price",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Alexander Rowan Price. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'Superstar05@gmail.com',
+  address: 'Seattle, Washington, United States - 98101 ',
+  github: 'https://github.com/Superstar051010-rep',
+  linkedIn: 'https://www.linkedin.com/in/alexander-rowan-price/',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/alexander-rowan-price',
+  devUsername: "Superstar051010",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
